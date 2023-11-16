e8bd38e9d8139a9d91f5e484c84b971228517654
LAPTOP-265UD690\Noahv <noahvdo@icloud.com>
2023-11-16 14:07:35 +0100

    Added pic and set up basic functions

--- 4.1/determine_loads.py
+++ 4.1/determine_loads.py
@@ -1,2 +1,45 @@
 print("hallo")
 print("hoi")
+
+
+# Load forces
+loads = {"x": 0, "y": 0, "z": 0}
+# Moment forces
+moments = {"x": 0, "y": 0, "z": 0}
+
+# time
+t = 0
+# change in time step dt
+dt = 0
+
+#
+# Dimensions of the lug
+#
+# Diameter of the holes of the lug
+D_1 = 0
+D_2 = 0
+
+# height of the lug
+w = 0
+# width of the lug
+t_1 = 0
+# width
+t_2 = 0
+# vehicle wall thickness
+t_3 = 0
+# Space between the holes
+h = 0
+# Number of holes
+n_holes = 2
+
+
+def gravitational_forces():
+    return
+
+
+def aerodynamic_forces():
+    return
+
+
+def magnetic_forces():
+    return
